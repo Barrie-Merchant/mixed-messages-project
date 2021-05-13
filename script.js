b2e6bd8a88aec02eac40d69843e3da66e9f99465
Barrie <barriemerchant@yahoo.co.uk>
2021-05-13 12:50:26 +0100

    Object and random number picker in place

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log('Welcome to Node');
+const movqs = [
+    {
+    film: 'The Godfather',
+    character: 'Don Corleone',
+    quote: "I'm gonna make him an offer he can't refuse"
+    },
+    {
+    film: 'Wizard of Oz',
+    character: 'Dorothy',
+    quote: "Toto, I have a feeling we're not in Kansas anymore. "  
+    },
+    {
+    film: 'Dirty Dancing',
+    character:'Johnny Castle',
+    quote: 'Nobody puts Baby in the corner.'
+    },
+    {
+    film: 'Titanic',
+    character: 'Jack Dawson',
+    qoute: "I'm the King of the World!!"
+    },
+    {
+    film: 'Scarface',
+    character: 'Tony Montana',
+    qoute: 'Say hello to my little friend!!'    
+    },
+    {
+    film: 'Dr No.',
+    character: 'James Bond',
+    qoute: 'Bond, James Bond'
+    },
+    {
+    film: 'The Dark Knight',
+    character: 'Joker',
+    qoute: 'Why so Serious?!!'
+    },
+    {
+    film: 'Endgame',
+    character: 'Tony Stark',
+    qoute: 'Love you 3000'   
+    }
+];
+
+let random = Math.floor(Math.random() * movqs.length);
+
