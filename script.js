846cb708c598bc488c255a377c457b29028d56d5
Barrie <barriemerchant@yahoo.co.uk>
2021-05-13 13:45:31 +0100

    Random functions working

--- script.js
+++ script.js
@@ -37,9 +37,41 @@ const movqs = [
     {
     film: 'Endgame',
     character: 'Tony Stark',
-    qoute: 'Love you 3000'   
+    qoute: 'Love you 3000'  
     }
 ];
 
-let random = Math.floor(Math.random() * movqs.length);
 
+
+
+let answer = () => {
+const random = Math.floor(Math.random() * movqs.length);
+switch(random) {
+    case 0:
+        return movqs[0];
+        break;
+        case 1:
+            return movqs[1];
+            break;
+            case 2:
+                return movqs[2];
+                break;
+                case 3:
+                    return movqs[3];
+                    break;
+                    case 4:
+                        return movqs[4];
+                        break;
+                        case 5 :
+                            return movqs[5];
+                            break;
+                            case 6:
+                                return movqs[6];
+                                break;
+                                case 7:
+                                    return movqs[7];
+                                    break;
+}
+}
+
+console.log(answer());
